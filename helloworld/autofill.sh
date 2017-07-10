965bdb6c4925ce71a40c9091a7922834bc19f0a5
VadzimTarasiuk <vad.tarasyuk@gmail.com>
2017-07-10 15:02:02 +0300

    Update autofill.sh

--- helloworld/autofill.sh
+++ helloworld/autofill.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 now=$(date +"%T");
-sed -i "s@Now is .*@Now is $now </p>@" ./src/main/webapp/index.html
+sed -i "s@Now is .*@Now is $now </p>@" ./jboss-eap/helloworld/src/main/webapp/index.html
